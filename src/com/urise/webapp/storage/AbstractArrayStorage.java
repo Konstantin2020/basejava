c168efd752e7af97a58997a7d16db7b3405a9277
Konstantin2020 <ykn79@mail.ru>
2021-08-18 11:04:42 +0500

    implement HW3

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,64 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public abstract class AbstractArrayStorage implements Storage{
+    protected static final int STORAGE_LIMIT = 10_000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    @Override
+    public final int size(){
+        return size;
+    }
+
+    @Override
+    public final void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    @Override
+    public final void update(Resume resume) {
+        int indexForUpdate = getIndex(resume.getUuid());
+        if (indexForUpdate >= 0) {
+            System.out.println("Update обновляет резюме " + storage[indexForUpdate].getUuid() + ".");
+            storage[indexForUpdate] = resume;
+        }
+    }
+
+    @Override
+    public final void delete(String uuid) {
+        int indexForDelete = getIndex(uuid);
+        if (indexForDelete >= 0) {
+            System.out.println("Резюме " + storage[indexForDelete].getUuid() + " удалено!");
+            storage[indexForDelete] = null;
+            if (size - (indexForDelete + 1) >= 0)
+                System.arraycopy(storage, indexForDelete + 1, storage, indexForDelete, size - (indexForDelete + 1));
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    @Override
+    public final Resume get(String uuid) {
+        int index = getIndex (uuid);
+        if (index < 0){
+            System.out.println("Resume " + uuid + " not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    @Override
+    public final Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public abstract void save(Resume resume);
+
+    protected abstract int getIndex(String uuid);
+}
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,74 +2,24 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage{
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void update(Resume resume) {
-        int indexForUpdate = checkInStorage(resume.getUuid());
-        if (indexForUpdate != -1) {
-            System.out.println("Update обновляет резюме " + storage[indexForUpdate].getUuid() + ".");
-            storage[indexForUpdate] = resume;
-        }
-    }
-
+    @Override
     public void save(Resume resume) {
-        if (size >= storage.length) {
+        if (size >= STORAGE_LIMIT) {
             System.out.println("Хранилище переполнено, количество занятых ячеек массива" + size + ".");
-        } else if (checkInStorage(resume.getUuid()) == -1) {
+        } else if (getIndex(resume.getUuid()) == -1) {
             System.out.println("Save сохраняет резюме " + resume.getUuid() + ".");
             storage[size] = resume;
             size++;
         }
     }
 
-    public Resume get(String uuid) {
-        int indexForGet = checkInStorage(uuid);
-        Resume resumeForGet = null;
-        if (indexForGet != -1) {
-            resumeForGet = storage[indexForGet];
-        }
-        return resumeForGet;
-    }
-
-    public void delete(String uuid) {
-        int indexForDelete = checkInStorage(uuid);
-        if (indexForDelete != -1) {
-            System.out.println("Резюме " + storage[indexForDelete].getUuid() + " удалено!");
-            storage[indexForDelete] = null;
-            if (size - (indexForDelete + 1) >= 0)
-                System.arraycopy(storage, indexForDelete + 1, storage, indexForDelete + 1 - 1, size - (indexForDelete + 1));
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        Resume[] allResume = new Resume[size];
-        System.arraycopy(storage, 0, allResume, 0, allResume.length);
-        return allResume;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public int checkInStorage(String uuid) {
+    @Override
+    protected int getIndex(String uuid) {
         int indexForCheck = -1;
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
@@ -85,4 +35,3 @@ public class ArrayStorage {
         return indexForCheck;
     }
 }
-
