045f9ca11cff40febcd37e48cf6de414fef9761a
Konstantin2020 <ykn79@mail.ru>
2021-12-24 23:35:57 +0500

    implement HW12 after checking 1

--- src/com/urise/webapp/MainStreams.java
+++ src/com/urise/webapp/MainStreams.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static java.lang.Math.pow;
 
@@ -16,7 +14,7 @@ public class MainStreams {
         System.out.println("Результат: " + minValue(new int[]{9, 2, 3, 7, 7, 9, 3}));
         System.out.println(" ");
 // вызов oddOrEven через через Streams
-        System.out.println("Результат: " + oddOrEven(new ArrayList<>(Arrays.asList(1, 2, 2, 1, 1))));
+        System.out.println("Результат: " + oddOrEven(new ArrayList<>(Arrays.asList(1, 2, 1, 1, 1))));
         System.out.println(" ");
 // вызов minValueSimple через for
         System.out.println("Результат: " + minValueSimple(new int[]{9, 2, 3, 7, 7, 9, 3}));
@@ -25,7 +23,7 @@ public class MainStreams {
         System.out.println("Результат: " + oddOrEvenSimple(Arrays.asList(2, 2, 3, 6, 8, 9, 5, 3, 1)));
     }
 
-// реализация minValue через Streams
+    // реализация minValue через Streams
     private static int minValue(int[] values) {
         System.out.println("Реализация minValue через Streams");
         System.out.print("Набор элементов: ");
@@ -33,31 +31,22 @@ public class MainStreams {
             System.out.print(i + " ");
         }
         System.out.println(" ");
-        return IntStream.range(0, Arrays.stream(values).distinct().sorted().toArray().length)
-                .map(i -> (int) (Math.pow(10, Arrays.stream(values).distinct().sorted().toArray().length - 1 - i) * Arrays.stream(values).distinct().sorted().toArray()[i]))
-                .sum();
+        return Arrays.stream(values).distinct().sorted().reduce(0, (result, element) -> result * 10 + element);
     }
 
-// реализация oddOrEven через через Streams
+    // реализация oddOrEven через через Streams
     private static List<Integer> oddOrEven(List<Integer> integers) {
         System.out.println("Реализация oddOrEven через Streams");
         System.out.println("Набор элементов: " + integers.toString());
-        System.out.println("Сумма элементов: " + integers.stream().mapToInt(a -> a).sum());
-        return integers.stream().flatMap(y -> {
-            if (integers.stream().mapToInt(a -> a).sum() % 2 == 0) {
-                if (y % 2 != 0) {
-                    System.out.println(y);
-                }
-            } else {
-                if (y % 2 == 0) {
-                    return Stream.of(y);
-                }
-            }
-            return null;
-        }).collect(Collectors.toList());
+        int totalSum = integers.stream().mapToInt(a -> a).sum();
+        System.out.println("Сумма элементов: " + totalSum);
+        return integers
+                .stream()
+                .filter(x -> ((totalSum % 2 != 0) && (x % 2 == 0)) || ((totalSum % 2 == 0) && !(x % 2 == 0)))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-// реализация minValue через for
+    // реализация minValue через for
     private static int minValueSimple(int[] values) {
         System.out.println("Реализация minValue через for");
         System.out.print("Набор элементов: ");
@@ -74,7 +63,7 @@ public class MainStreams {
         return totalVal;
     }
 
-// реализация oddOrEven через for и if
+    // реализация oddOrEven через for и if
     private static List<Integer> oddOrEvenSimple(List<Integer> integers) {
         System.out.println("Реализация oddOrEven через for и if");
         System.out.println("Набор элементов: " + integers.toString());
